95c2f46918940c65b3365f99faa0ef21161a0e08
joeyklee <jsphknglee@gmail.com>
2020-03-02 11:36:46 -0500

    updates config

--- config.js
+++ config.js
@@ -2,6 +2,4 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/empty-tree-db',
-  MONGODB_USERNAME: process.env.MONGODB_USERNAME || 'joey',
-  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD || 'supersecretpassword',
 }
